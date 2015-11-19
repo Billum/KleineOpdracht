74c6836c5ba1806ce393e9362f7d7a7ef7d2821e
Billum <tomas.billum@outlook.com>
2015-11-19 13:34:36 +0100

    libraries added

--- KleineOpdracht/Program.cs
+++ KleineOpdracht/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.SolverFoundation;
+using Microsoft.SolverFoundation.Common;
+using Microsoft.SolverFoundation.Services;
 
 namespace KleineOpdracht
 {
