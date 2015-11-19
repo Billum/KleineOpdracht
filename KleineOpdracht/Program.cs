f7a797ca5b0f27a8a10157d086ecd8deb1608400
Billum <tomas.billum@outlook.com>
2015-11-19 14:41:28 +0100

    shit

--- KleineOpdracht/Program.cs
+++ KleineOpdracht/Program.cs
@@ -16,48 +16,58 @@ namespace KleineOpdracht
             SolverContext context = SolverContext.GetContext();
             Model model = context.CreateModel();
 
-            Decision project1 = new Decision(Domain.RealNonnegative, "Project 1");
-            Decision project2 = new Decision(Domain.RealNonnegative, "Project 2");
-            Decision project3 = new Decision(Domain.RealNonnegative, "Project 3");
-            Decision project4 = new Decision(Domain.RealNonnegative, "Project 4");
-            Decision project5 = new Decision(Domain.RealNonnegative, "Project 5");
-            Decision project6 = new Decision(Domain.RealNonnegative, "Project 6");
-            Decision jaar1 = new Decision(Domain.IntegerNonnegative, "Jaar 1");
-            Decision jaar2 = new Decision(Domain.IntegerNonnegative, "Jaar 2");
-            Decision jaar3 = new Decision(Domain.IntegerNonnegative, "Jaar 3");
-            Decision jaar4 = new Decision(Domain.IntegerNonnegative, "Jaar 4");
-
             Decision[,] x = new Decision[7,5];
-            x[1, 1] = new Decision(Domain.RealNonnegative, "(1,1)");
-            x[1, 2] = new Decision(Domain.RealNonnegative, "(1,2)");
-            x[1, 3] = new Decision(Domain.RealNonnegative, "(1,3)");
-            x[1, 4] = new Decision(Domain.RealNonnegative, "(1,4)");
-            x[2, 1] = new Decision(Domain.RealNonnegative, "(2,1)");
-            x[2, 2] = new Decision(Domain.RealNonnegative, "(2,2)");
-            x[2, 3] = new Decision(Domain.RealNonnegative, "(2,3)");
-            x[2, 4] = new Decision(Domain.RealNonnegative, "(2,4)");
-            x[3, 1] = new Decision(Domain.RealNonnegative, "(3,1)");
-            x[3, 2] = new Decision(Domain.RealNonnegative, "(3,2)");
-            x[3, 3] = new Decision(Domain.RealNonnegative, "(3,3)");
-            x[3, 4] = new Decision(Domain.RealNonnegative, "(3,4)");
-            x[4, 1] = new Decision(Domain.RealNonnegative, "(4,1)");
-            x[4, 2] = new Decision(Domain.RealNonnegative, "(4,2)");
-            x[4, 3] = new Decision(Domain.RealNonnegative, "(4,3)");
-            x[4, 4] = new Decision(Domain.RealNonnegative, "(4,4)");
-            x[5, 1] = new Decision(Domain.RealNonnegative, "(5,1)");
-            x[5, 2] = new Decision(Domain.RealNonnegative, "(5,2)");
-            x[5, 3] = new Decision(Domain.RealNonnegative, "(5,3)");
-            x[5, 4] = new Decision(Domain.RealNonnegative, "(5,4)");
-            x[6, 1] = new Decision(Domain.RealNonnegative, "(6,1)");
-            x[6, 2] = new Decision(Domain.RealNonnegative, "(6,2)");
-            x[6, 3] = new Decision(Domain.RealNonnegative, "(6,3)");
-            x[6, 4] = new Decision(Domain.RealNonnegative, "(6,4)");
-            Decision y = new Decision(Domain.RealNonnegative, "Geld uitzetten");
-            Decision z = new Decision(Domain.RealNonnegative, "Lening");
-            Decision w = new Decision(Domain.RealNonnegative, "Schuld aan derden");
+            Decision[] y = new Decision[5];
+            Decision[] z = new Decision[5];
+            Decision[] w = new Decision[5];
 
+            model.AddDecision(x[1, 1] = new Decision(Domain.RealNonnegative, "x(1,1)"));
+            model.AddDecision(x[1, 2] = new Decision(Domain.RealNonnegative, "x(1,2)"));
+            model.AddDecision(x[1, 3] = new Decision(Domain.RealNonnegative, "x(1,3)"));
+            model.AddDecision(x[1, 4] = new Decision(Domain.RealNonnegative, "x(1,4)"));
+            model.AddDecision(x[2, 1] = new Decision(Domain.RealNonnegative, "x(2,1)"));
+            model.AddDecision(x[2, 2] = new Decision(Domain.RealNonnegative, "x(2,2)"));
+            model.AddDecision(x[2, 3] = new Decision(Domain.RealNonnegative, "x(2,3)"));
+            model.AddDecision(x[2, 4] = new Decision(Domain.RealNonnegative, "x(2,4)"));
+            model.AddDecision(x[3, 1] = new Decision(Domain.RealNonnegative, "x(3,1)"));
+            model.AddDecision(x[3, 2] = new Decision(Domain.RealNonnegative, "x(3,2)"));
+            model.AddDecision(x[3, 3] = new Decision(Domain.RealNonnegative, "x(3,3)"));
+            model.AddDecision(x[3, 4] = new Decision(Domain.RealNonnegative, "x(3,4)"));
+            model.AddDecision(x[4, 1] = new Decision(Domain.RealNonnegative, "x(4,1)"));
+            model.AddDecision(x[4, 2] = new Decision(Domain.RealNonnegative, "x(4,2)"));
+            model.AddDecision(x[4, 3] = new Decision(Domain.RealNonnegative, "x(4,3)"));
+            model.AddDecision(x[4, 4] = new Decision(Domain.RealNonnegative, "x(4,4)"));
+            model.AddDecision(x[5, 1] = new Decision(Domain.RealNonnegative, "x(5,1)"));
+            model.AddDecision(x[5, 2] = new Decision(Domain.RealNonnegative, "x(5,2)"));
+            model.AddDecision(x[5, 3] = new Decision(Domain.RealNonnegative, "x(5,3)"));
+            model.AddDecision(x[5, 4] = new Decision(Domain.RealNonnegative, "x(5,4)"));
+            model.AddDecision(x[6, 1] = new Decision(Domain.RealNonnegative, "x(6,1)"));
+            model.AddDecision(x[6, 2] = new Decision(Domain.RealNonnegative, "x(6,2)"));
+            model.AddDecision(x[6, 3] = new Decision(Domain.RealNonnegative, "x(6,3)"));
+            model.AddDecision(x[6, 4] = new Decision(Domain.RealNonnegative, "x(6,4)"));
 
-            model.AddDecisions(y,z,w);
+            model.AddDecision(y[1] = new Decision(Domain.RealNonnegative, "y(1)"));
+            model.AddDecision(y[2] = new Decision(Domain.RealNonnegative, "y(2)"));
+            model.AddDecision(y[3] = new Decision(Domain.RealNonnegative, "y(3)"));
+            model.AddDecision(y[4] = new Decision(Domain.RealNonnegative, "y(4)"));
+
+            model.AddDecision(z[1] = new Decision(Domain.RealNonnegative, "z(1)"));
+            model.AddDecision(z[2] = new Decision(Domain.RealNonnegative, "z(2)"));
+            model.AddDecision(z[3] = new Decision(Domain.RealNonnegative, "z(3)"));
+            model.AddDecision(z[4] = new Decision(Domain.RealNonnegative, "z(4)"));
+
+            model.AddDecision(w[1] = new Decision(Domain.RealNonnegative, "w(1)"));
+            model.AddDecision(w[2] = new Decision(Domain.RealNonnegative, "w(2)"));
+            model.AddDecision(w[3] = new Decision(Domain.RealNonnegative, "w(3)"));
+            model.AddDecision(w[4] = new Decision(Domain.RealNonnegative, "w(4)"));
+
+            var maxlening = 50;
+
+            model.AddConstraints("Lening", z[1] <= maxlening,
+                                                    z[2] <= maxlening,
+                                                    z[3] <= maxlening,
+                                                    z[4] <= maxlening);
+            model.AddConstraint("Schuld aan derden", w[1] + w[2] + w[3] + w[4] <= 10);
         }
     }
 }
