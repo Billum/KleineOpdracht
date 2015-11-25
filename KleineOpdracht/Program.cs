168d8e1bf6eebd2b3a7971d99e379c83ab5addc5
Billum <tomas.billum@outlook.com>
2015-11-25 12:44:11 +0100

    more readible

--- KleineOpdracht/Program.cs
+++ KleineOpdracht/Program.cs
@@ -49,24 +49,24 @@ namespace KleineOpdracht
             Decision[] z = new Decision[3]; // geld uitzetten
             Decision[] w = new Decision[3]; // schuld aan derden
 
-            model.AddDecision(x[0] = new Decision(Domain.RealNonnegative, "x_0"));
-            model.AddDecision(x[1] = new Decision(Domain.RealNonnegative, "x_1"));
-            model.AddDecision(x[2] = new Decision(Domain.RealNonnegative, "x_2"));
-            model.AddDecision(x[3] = new Decision(Domain.RealNonnegative, "x_3"));
-            model.AddDecision(x[4] = new Decision(Domain.RealNonnegative, "x_4"));
-            model.AddDecision(x[5] = new Decision(Domain.RealNonnegative, "x_5"));
+            model.AddDecision(x[0] = new Decision(Domain.RealNonnegative, "project___1"));
+            model.AddDecision(x[1] = new Decision(Domain.RealNonnegative, "project___2"));
+            model.AddDecision(x[2] = new Decision(Domain.RealNonnegative, "project___3"));
+            model.AddDecision(x[3] = new Decision(Domain.RealNonnegative, "project___4"));
+            model.AddDecision(x[4] = new Decision(Domain.RealNonnegative, "project___5"));
+            model.AddDecision(x[5] = new Decision(Domain.RealNonnegative, "project___6"));
 
-            model.AddDecision(y[0] = new Decision(Domain.RealNonnegative, "y_0"));
-            model.AddDecision(y[1] = new Decision(Domain.RealNonnegative, "y_1"));
-            model.AddDecision(y[2] = new Decision(Domain.RealNonnegative, "y_2"));
+            model.AddDecision(y[0] = new Decision(Domain.RealNonnegative, "lening____1"));
+            model.AddDecision(y[1] = new Decision(Domain.RealNonnegative, "lening____2"));
+            model.AddDecision(y[2] = new Decision(Domain.RealNonnegative, "lening____3"));
 
-            model.AddDecision(z[0] = new Decision(Domain.RealNonnegative, "z_0"));
-            model.AddDecision(z[1] = new Decision(Domain.RealNonnegative, "z_1"));
-            model.AddDecision(z[2] = new Decision(Domain.RealNonnegative, "z_2"));
+            model.AddDecision(z[0] = new Decision(Domain.RealNonnegative, "geld_uitz_1"));
+            model.AddDecision(z[1] = new Decision(Domain.RealNonnegative, "geld_uitz_2"));
+            model.AddDecision(z[2] = new Decision(Domain.RealNonnegative, "geld_uitz_3"));
 
-            model.AddDecision(w[0] = new Decision(Domain.RealNonnegative, "w_0"));
-            model.AddDecision(w[1] = new Decision(Domain.RealNonnegative, "w_1"));
-            model.AddDecision(w[2] = new Decision(Domain.RealNonnegative, "w_2"));
+            model.AddDecision(w[0] = new Decision(Domain.RealNonnegative, "schuld____1"));
+            model.AddDecision(w[1] = new Decision(Domain.RealNonnegative, "schuld____2"));
+            model.AddDecision(w[2] = new Decision(Domain.RealNonnegative, "schuld____3"));
 
             var a0 = -50 * x[0] - 100 * x[1] - 60 * x[2] - 50 * x[3] - 170 * x[4] - 16 * x[5];
             var a1 = -80 * x[0] - 50 * x[1] - 60 * x[2] - 100 * x[3] - 40 * x[4] - 25 * x[5];
@@ -76,13 +76,13 @@ namespace KleineOpdracht
                 x[0] <= 1, x[1] <= 1, x[2] <= 1,
                 x[3] <= 1, x[4] <= 1, x[5] <= 1);
 
-            model.AddConstraints("lening", 
+            model.AddConstraints("leningen", 
                 y[0] <= 50,
                 y[1] <= 50 + 0.20 * (300 * a0 + y[0]),
                 y[2] <= 50 + 0.20 * (400 * a1 + y[1] + z[1]));
 
             model.AddConstraints("geld_uitzetten",
-                z[0] <= 300 + y[0] - 300 * a0 - w[1],
+                z[0] <= 300 + y[0] - 300 * a0 - w[0],
                 z[1] <= 100 + y[1] + 1.08 * z[0] - 100 * a1 - w[1],
                 z[2] <= 200 + y[2] + 1.08 * z[1] - 200 * a2 - w[2]);
 
@@ -102,6 +102,15 @@ namespace KleineOpdracht
                 a0 <= 300,
                 a1 <= 400 - a1,
                 a2 <= 600 - (a1 + a2));
+
+            model.AddGoal("winst", GoalKind.Maximize,
+                150 * x[0] + 210 * x[1] + 220 * x[2] + 350 * x[3] + 200 * x[4] + 100 * x[5] -
+                (y[0] + y[1] + y[2]) * 1.12 -
+                (z[0] + z[1] + z[2]) * 1.08 -
+                (w[0] + w[1] + w[2]) - (10 - (w[0] + w[1] + w[2])) * 0.11);
+
+            Solution sol = context.Solve(new SimplexDirective());
+            Console.WriteLine(sol.GetReport());
         }
     }
 }
