7f30e83751608200abfb4f28a8c9fc6b35d201a5
Billum <tomas.billum@outlook.com>
2015-11-26 11:08:33 +0100

    it checks out!

--- KleineOpdracht/Program.cs
+++ KleineOpdracht/Program.cs
@@ -37,10 +37,10 @@ namespace KleineOpdracht
             var context = SolverContext.GetContext();
             var model = context.CreateModel();
 
-            var x = new Decision[6];
-            var y = new Decision[4];
-            var z = new Decision[4];
-            var w = new Decision[4];
+            var x = new Decision[6]; // projecten
+            var y = new Decision[4]; // leningen
+            var z = new Decision[4]; // geld uitzetten
+            var w = new Decision[4]; // schuld aan derden
 
             model.AddDecision(x[0] = new Decision(Domain.RealNonnegative, "Project_____1"));
             model.AddDecision(x[1] = new Decision(Domain.RealNonnegative, "Project_____2"));
@@ -69,6 +69,8 @@ namespace KleineOpdracht
             var a2 = 20 * x[0] - 20 * x[1] - 60 * x[2] - 150 * x[3] + 50 * x[4] - 40 * x[5];
             var a3 = 150 * x[0] + 210 * x[1] + 220 * x[2] + 350 * x[3] + 200 * x[4] + 100 * x[5];
 
+            var a2_2 = -20 * x[1] - 60 * x[2] - 150 * x[3] - 40 * x[5]; // alleen investeringen jaar 3
+
             model.AddConstraints(null,
                 x[0] >= 0, x[1] >= 0, x[2] >= 0,
                 x[3] >= 0, x[4] >= 0, x[5] >= 0,
@@ -79,18 +81,24 @@ namespace KleineOpdracht
                 w[0] >= 0, w[1] >= 0, w[2] >= 0, w[3] >= 0
             );
 
+            var restKapitaal_0 = 300 + a0 + y[0] - w[0] - z[0];
+            var restKapitaal_1 = 100 + restKapitaal_0 + a1
+                                 + y[1] - y[0] * 1.12
+                                 - w[1] - 0.11 * (10 - w[0])
+                                 - z[1] + 1.08 * z[0];
+
             model.AddConstraints(null,
                 -a0 <= 300
                         + y[0]
                         - w[0]
                         - z[0],
-                -a1 <= 100 + a0
+                -a1 <= 100 + restKapitaal_0
                         + y[1] - y[0] * 1.12
-                        - w[1] - 0.11 * (10 - w[0])
+                        - w[1] - 0.11 * (10 - sums(w, 1))
                         - z[1] + 1.08 * z[0],
-                -a2 <= 200 + a0 + a1
+                -a2 <= 200 + restKapitaal_1
                         + y[2] - y[1] * 1.12
-                        - w[2] - 0.11 * (10 - w[1])
+                        - w[2] - 0.11 * (10 - sums(w, 2))
                         - z[2] + 1.08 * z[1]
             );
 
@@ -98,29 +106,33 @@ namespace KleineOpdracht
                 y[0] <= 50,
                 y[1] <= 50 - 0.2 * a0,
                 y[2] <= 50 - 0.2 * (a0 + a1),
-                y[3] <= 50 - 0.2 * (a0 + a1 + a2)
+                y[3] <= 50 - 0.2 * (a0 + a1 + a2_2)
             );
 
             model.AddConstraints(null,
                 w[0] <= 10,
-                w[1] <= 10 + 0.11 * (10 - w[0]),
-                w[2] <= 10 + 0.11 * (10 - w[0] - w[1]),
-                w[3] <= 10 + 0.11 * (10 - w[0] - w[1] - w[2]),
+                w[1] <= 10 + 0.11 * (10 - sums(w, 1)),
+                w[2] <= 10 + 0.11 * (10 - sums(w, 2)),
+                w[3] <= 10 + 0.11 * (10 - sums(w, 3)),
                 sums(w, 4) <= 10 * Math.Pow(1.11, 3)
             );
 
             model.AddGoal("Goal", GoalKind.Maximize,
-                a3                                        // inkomsten jaar 4  
-                - 1.12 * y[2] + y[3]                      // lening jaar 3 aflossen + lening jaar
-                - sums(w, 3) - 0.11 * (10 - sums(w, 3))   // schuld aan derden aflossen - rente schuld aan derden
-                - z[3] + 1.08 * z[2]                      // geld uitgezet jaar 4 + rente jaar 3
+                a3                                          // inkomsten jaar 4  
+                - 1.12 * y[2]                               // lening jaar 3 aflossen
+                - 1.08 * z[2]                               // rente uitgezet geld jaar 3
+                - w[3] - 0.11 * (10 - sums(w, 3))           // schuld aan derden aflossen - rente schuld aan derden
             );
 
             Solution solution = context.Solve(new SimplexDirective());
             Console.WriteLine(solution.GetReport());
 
+            Console.WriteLine(x[3]);
+            Console.WriteLine(z[0]);
+            Console.WriteLine(y[1]);
+
             // check the solution by simulating
-            Simulate(d2d(x), d2d(y), d2d(z), d2d(w));
+            //Simulate(d2d(x), d2d(y), d2d(z), d2d(w));
         }
 
         public static double[] d2d(Decision[] d)
